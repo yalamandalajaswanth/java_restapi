244414cee1b3f969fb781ffdb63a57715679ca52
yalamandalajaswanth <yalamandalajaswanth@gmail.com>
2023-07-10 17:15:39 -0500

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,27 @@
 package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String jdbcUrl = "jdbc:h2:mem:testdb";
+        String username = "sa";
+        String password = "";
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            Statement statement = connection.createStatement();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            // Execute SQL statement to create the database
+            String Sqlquery = "CREATE SCHEMA test";
+            statement.executeUpdate(Sqlquery);
+            System.out.println("Database 'test' created successfully.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
